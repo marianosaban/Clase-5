29e27511cc58b12771b153f22d720133c136d8be
marianosaban <75099959+marianosaban@users.noreply.github.com>
2021-03-08 23:42:56 -0300

    Primera entrega proyecto final

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ function tipo(nombre,transmision){
     alert("Bueno, "+nombre+" hagamos tu bicicleta "+transmision)
 }
 var nombre = prompt("¿Cómo te llamas?")
-alert("Hola "+nombre+" esta pagina es un simulador que te permitirá presupuestar cuanto te saldria armar tu bici con nosotres.")
-alert("¿Qué tipo de bici queres armar?")
 var transmision = prompt("¿piñon fijo o libre?" )
 tipo(nombre,transmision)
 class bici {
@@ -18,6 +16,6 @@ class bici {
     }
 }
 
-var mibici = new bici(color=prompt("De que color sera tu bici"),transmision=transmision,manubrio=prompt("¿Qué tipo de manubrio?"),ruedaDel=prompt("¿La rueda delantera: doble pared, perfil alto o triple pared?"),ruedaTras=prompt("¿La rueda Trasera: doble pared, perfil alto o triple pared?"))
+var mibici = new bici(color=prompt("De que color sera tu bici"),transmision=transmision,manubrio=prompt("¿Qué tipo de manubrio?"),ruedaDel=prompt("¿La rueda delantera: normal, perfil alto o triple pared?"),ruedaTras=prompt("¿La rueda Trasera: normal, perfil alto o triple pared?"))
 //getMiBici = function () {return this.color,this.transmision,this.manubrio,this.ruedaDel,this.ruedaTras}
 console.log(nombre +" Entonces vamos a hacer una bici " +transmision+ " de color "+this.color+" con un manubrio "+this.manubrio+". La rueda delantera "+this.ruedaDel+" y la trasera "+this.ruedaTras)
