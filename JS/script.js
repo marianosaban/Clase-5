0f12cdb1119a23953c49e87100061e0f70a1c38d
marianosaban <75099959+marianosaban@users.noreply.github.com>
2021-07-06 00:29:58 -0300

    Entrega funcional cotizando

--- JS/script.js
+++ JS/script.js
@@ -1,37 +1,5 @@
-
-var arrayImagenes = ["IMAGENES/bici_cuadro_azul.png" , "IMAGENES/bici_cuadro_blanco.png", "IMAGENES/bici_cuadro_negro.png", "IMAGENES/bici_cuadro_rojo.png", "IMAGENES/bici_cuadro_verde.png"]
-var imagenActual = 0;
-function cambiarImagen(){
-    foto = document.getElementById("cambiaFoto");
-    var boton = this.id;
-    if (boton === "botonNegro") {
-            foto.src = "IMAGENES/bici_cuadro_negro.png";
-            actualizar()
-        } else if (boton === "botonBlanco") {
-            foto.src = "IMAGENES/bici_cuadro_blanco.png";
-            actualizar()
-        }
-        else if (boton === "botonRojo") {
-            foto.src = "IMAGENES/bici_cuadro_rojo.png";
-            actualizar()
-        }
-        else if (boton === "botonVerde") {
-            foto.src = "IMAGENES/bici_cuadro_verde.png";
-            actualizar()
-        }
-        else if (boton === "botonAzul") {
-            foto.src = "IMAGENES/bici_cuadro_azul.png";
-            actualizar()
-        }
-    }
-
-$("#botonNegro").click(cambiarImagen);
-$("#botonBlanco").click(cambiarImagen);
-$("#botonRojo").click(cambiarImagen);
-$("#botonVerde").click(cambiarImagen);
-$("#botonAzul").click(cambiarImagen);
-
-
+var colorActual= "negra"
+var precio = 50000;
 let transmision = $("#transmision");
 transmision.change(actualizar);
 let ruedaDel = $('[name="RuedaDel"]');
@@ -39,14 +7,62 @@ ruedaDel.change(actualizar);
 let ruedaTras = $('[name="RuedaTras"]');
 ruedaTras.change(actualizar);
 
-
+function cambiarImagen(){
+    foto = document.getElementById("cambiaFoto");
+    var boton = this.id;
+    if (boton === "botonNegro") {
+            foto.src = "IMAGENES/bici_cuadro_negro.png";
+            colorActual="negra";
+            actualizar()
+        }
+        else if (boton === "botonBlanco") {
+            foto.src = "IMAGENES/bici_cuadro_blanco.png";
+            colorActual="blanca";
+            actualizar()
+        }
+        else if (boton === "botonRojo") {
+            foto.src = "IMAGENES/bici_cuadro_rojo.png";
+            colorActual="roja";
+            actualizar()
+        }
+        else if (boton === "botonVerde") {
+            foto.src = "IMAGENES/bici_cuadro_verde.png";
+            colorActual="verde";
+            actualizar()
+        }
+        else if (boton === "botonAzul") {
+            foto.src = "IMAGENES/bici_cuadro_azul.png";
+            colorActual="azul";
+            actualizar()
+        }
+    }
+$("#botonNegro").click(cambiarImagen);
+$("#botonBlanco").click(cambiarImagen);
+$("#botonRojo").click(cambiarImagen);
+$("#botonVerde").click(cambiarImagen);
+$("#botonAzul").click(cambiarImagen);
 function actualizar(){
+    precio = 50000;
     var transmisionElegida= $("#transmision").val();
+    if (transmisionElegida === "con 6 cambios"){
+        precio=precio+20000
+    };
     var ruedaDelElegida= $('[name="RuedaDel"]:checked').val();
+    if (ruedaDelElegida === "triple pared"){
+        precio=precio+2000
+    }
+    else if(ruedaDelElegida === "perfil alto"){
+        precio=precio+5000
+    }
     var ruedaTrasElegida=$('[name="RuedaTras"]:checked').val();
-    $("#resultado").html("Cotizaremos una bicicleta " + transmisionElegida + ", con una rueda delantera " + ruedaDelElegida + " y una rueda trasera " + ruedaTrasElegida);
+    if (ruedaTrasElegida === "triple pared"){
+        precio=precio+2000
+    }
+    else if(ruedaTrasElegida === "perfil alto"){
+        precio=precio+5000
+    }
+    $("#resultado").html("Cotizaremos una bicicleta " + colorActual+ ", "+ transmisionElegida + ", con una rueda delantera " + ruedaDelElegida + " y una rueda trasera " + ruedaTrasElegida);
+    $("#resultadoPesos").html("el valor total de tu configuracion actual es de $" +precio );
     $("#cambios").fadeIn(500);
     $("#cambios").fadeOut(500);
-    
-
 }
